2eeaa00feb2b3ecc16346d4a21480e43a01bfe5a
Michal Kimle <kimle.michal@gmail.com>
2017-08-17 15:23:39 +0200

    Add OpenNebula user autocreate and token generation

--- app/lib/connectors/opennebula/user_handler.rb
+++ app/lib/connectors/opennebula/user_handler.rb
@@ -21,13 +21,27 @@ module Connectors
         handle_opennebula_error do
           user.allocate(username, password, auth)
           user.chgrp group.id
+          user.info
         end
+
+        user
       end
 
       def add_group(user, group)
         return if user.groups.include? group.id
 
         handle_opennebula_error { user.addgroup group.id }
+      end
+
+      def update(user, template)
+        handle_opennebula_error { user.update(template, true) }
+      end
+
+      def token(username, group, expiration)
+        user_alloc = OpenNebula::User.build_xml
+        user = OpenNebula::User.new(user_alloc, client)
+
+        handle_opennebula_error { user.login(username, '', (expiration - Time.now.to_i), group.id) }
       end
     end
   end
