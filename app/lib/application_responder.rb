5ae0b1f646f284303e00215b9e36e1be59bba9d5
Michal Kimle <kimle.michal@gmail.com>
2017-08-17 15:18:10 +0200

    Fix application responder to use request format for response

--- app/lib/application_responder.rb
+++ app/lib/application_responder.rb
@@ -2,7 +2,11 @@ class ApplicationResponder < ActionController::Responder
   # Redirects resources to the collection path (index action) instead
   # of the resource path (show action) for POST/PUT/DELETE requests.
   # include Responders::CollectionResponder
-  def to_json
+  def format
+    request.format.symbol
+  end
+
+  def respond
     display resource
   end
 end
