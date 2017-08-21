fd653514dd33715ad006bdbb7733d7d25318c5eb
Dusan Baran <dbaran@hotmail.sk>
2017-08-21 12:43:52 +0000

    Remove duplications in federation controller

--- app/controllers/v3/auth/federation_controller.rb
+++ app/controllers/v3/auth/federation_controller.rb
@@ -5,13 +5,17 @@ module V3
       include TokenRespondable
 
       def oidc
-        @credentials = ::Auth::Oidc.unified_credentials(ENV.select { |name| name.start_with?('OIDC') })
-        headers[x_subject_token_header_key] = Utils::Tokenator.to_token(@credentials.to_hash)
-        respond_with token_response
+        auth_response(::Auth::Oidc, 'OIDC')
       end
 
       def voms
-        @credentials = ::Auth::Voms.unified_credentials(ENV.select { |name| name.start_with?('GRST', 'SSL') })
+        auth_response(::Auth::Voms, 'SSL', 'GRST')
+      end
+
+      private
+
+      def auth_response(type, *filters)
+        @credentials = type.unified_credentials(ENV.select { |name| name.start_with?(*filters) })
         headers[x_subject_token_header_key] = Utils::Tokenator.to_token(@credentials.to_hash)
         respond_with token_response
       end
