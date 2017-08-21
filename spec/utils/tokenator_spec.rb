e3acd34f00314ea65f40004662d31d7d5712ea3a
Dusan Baran <dbaran@hotmail.sk>
2017-08-21 10:45:49 +0000

    Tokenator can now tokenize string
    Also checks if argument is not nil

--- spec/utils/tokenator_spec.rb
+++ spec/utils/tokenator_spec.rb
@@ -5,6 +5,10 @@ describe Utils::Tokenator, type: :model do
     it 'wont raise error' do
       expect { described_class.to_token(name: 'John') }.not_to raise_error
     end
+
+    it 'will raise error' do
+      expect { described_class.to_token(nil) }.to raise_error(Errors::AuthenticationError)
+    end
   end
 
   describe '#from_token' do
@@ -15,6 +19,18 @@ describe Utils::Tokenator, type: :model do
 
     it 'will raise error' do
       expect { described_class.from_token('  45 czxv12xc3=/`vx""') }.to raise_error(Errors::AuthenticationError)
+    end
+
+    it 'will return same string' do
+      input = 'dusan is the best'
+      origin = described_class.to_token(input)
+      expect(described_class.from_token(origin, parse: false)).to eq(input)
+    end
+
+    it 'will return same hash' do
+      input = { name: 'dusan', surname: 'awesome' }
+      origin = described_class.to_token(input)
+      expect(described_class.from_token(origin)).to eq(input)
     end
   end
 end
