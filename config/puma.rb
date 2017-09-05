d8c2d836fbc1107baef34c7d87199abbd6f0f746
Dusan Baran <work.dusanbaran@gmail.com>
2017-09-05 11:10:50 +0000

    Puma runs in single-threaded multi-process mode
    fixes #33

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,12 @@
+require 'concurrent'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 1 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
@@ -15,7 +17,8 @@ bind ENV.fetch('BIND') { "tcp://#{puma_host}:#{puma_port}" }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV') { 'development' }
+env = ENV.fetch('RAILS_ENV') { 'development' }
+environment env
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -23,7 +26,8 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+default_concurrency = env == 'production' ? Concurrent.processor_count * 2 : 1
+workers ENV.fetch('WEB_CONCURRENCY') { default_concurrency }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -32,7 +36,7 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
